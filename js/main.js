aa6a629b9aa29e866b8c97e680d40a473de4498b
MaedreDergau <161196056+MaedreDergau@users.noreply.github.com>
2025-01-05 12:36:31 +0300

    Vupdates

--- js/main.js
+++ js/main.js
@@ -1,162 +1,162 @@
-// Navbar
-const navbar = document.querySelector('#header');
+// // Navbar
+// const navbar = document.querySelector('#header');
 
-// // Scroll event
-window.addEventListener('scroll', () => {
-  if (window.scrollY > 70) {
-    navbar.classList.add('navbar-scroll');
-  } else {
-    navbar.classList.remove('navbar-scroll');
-  }
-});
+// // // Scroll event
+// window.addEventListener('scroll', () => {
+//   if (window.scrollY > 70) {
+//     navbar.classList.add('navbar-scroll');
+//   } else {
+//     navbar.classList.remove('navbar-scroll');
+//   }
+// });
 
-// // Hamburger Menu 
-const hamburger = document.querySelector('#nav-button');
-const navMenu = document.querySelector('#burger-menu');
+// // // Hamburger Menu 
+// const hamburger = document.querySelector('#nav-button');
+// const navMenu = document.querySelector('#burger-menu');
 
-// //  Events - Hamburger Menu
-hamburger.addEventListener('click', () => {
-    if (navMenu.classList.contains('hidden')) {
-        navMenu.classList.remove('hidden');
-    } else {
-        navMenu.classList.add('hidden');
-    }
-});
-
-
-// Our Classes Section
-
-const yoga = document.querySelector('.yoga');
-const yogaBtn = document.querySelector('#yoga-btn');
-
-const group = document.querySelector('.group');
-const groupBtn = document.querySelector('#group-btn');
-
-const solo = document.querySelector('.solo');
-const soloBtn = document.querySelector('#solo-btn');
-
-const stretch = document.querySelector('.stretch');
-const stretchBtn = document.querySelector('#stretch-btn');
-
-const classesBtns = document.querySelector('.classes-btns');
+// // //  Events - Hamburger Menu
+// hamburger.addEventListener('click', () => {
+//     if (navMenu.classList.contains('hidden')) {
+//         navMenu.classList.remove('hidden');
+//     } else {
+//         navMenu.classList.add('hidden');
+//     }
+// });
 
 
-// Our Classes Section Events
-document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('yoga-btn').click();
-  });
+// // Our Classes Section
 
-classesBtns.addEventListener('click', (e) => {
-  const id = e.target.id;
-  switch (id) {
-    case 'yoga-btn':
-      yoga.classList.remove('hidden');
-      yogaBtn.classList.add('bg-orange')
+// const yoga = document.querySelector('.yoga');
+// const yogaBtn = document.querySelector('#yoga-btn');
 
-      group.classList.add('hidden');
-      groupBtn.classList.remove('bg-orange')
+// const group = document.querySelector('.group');
+// const groupBtn = document.querySelector('#group-btn');
 
-      solo.classList.add('hidden');
-      soloBtn.classList.remove('bg-orange')
+// const solo = document.querySelector('.solo');
+// const soloBtn = document.querySelector('#solo-btn');
 
-      stretch.classList.add('hidden');      
-      stretchBtn.classList.remove('bg-orange')
-      break;
-    case 'group-btn':
-        yoga.classList.add('hidden');
-        yogaBtn.classList.remove('bg-orange')
+// const stretch = document.querySelector('.stretch');
+// const stretchBtn = document.querySelector('#stretch-btn');
 
-        group.classList.remove('hidden');
-        groupBtn.classList.add('bg-orange');
+// const classesBtns = document.querySelector('.classes-btns');
 
-        solo.classList.add('hidden');
-        soloBtn.classList.remove('bg-orange')
 
-        stretch.classList.add('hidden');
-        stretchBtn.classList.remove('bg-orange');
-        break;
-    case 'solo-btn':
-        yoga.classList.add('hidden');
-        yogaBtn.classList.remove('bg-orange')
+// // Our Classes Section Events
+// document.addEventListener('DOMContentLoaded', () => {
+//     document.getElementById('yoga-btn').click();
+//   });
 
-        group.classList.add('hidden');
-        groupBtn.classList.remove('bg-orange')
+// classesBtns.addEventListener('click', (e) => {
+//   const id = e.target.id;
+//   switch (id) {
+//     case 'yoga-btn':
+//       yoga.classList.remove('hidden');
+//       yogaBtn.classList.add('bg-orange')
 
-        solo.classList.remove('hidden');
-        soloBtn.classList.add('bg-orange')
+//       group.classList.add('hidden');
+//       groupBtn.classList.remove('bg-orange')
 
-        stretch.classList.add('hidden');
-        stretchBtn.classList.remove('bg-orange');
-        break;
-    case 'stretch-btn':
-        yoga.classList.add('hidden');
-        yogaBtn.classList.remove('bg-orange')
+//       solo.classList.add('hidden');
+//       soloBtn.classList.remove('bg-orange')
 
-        group.classList.add('hidden');
-        groupBtn.classList.remove('bg-orange')
+//       stretch.classList.add('hidden');      
+//       stretchBtn.classList.remove('bg-orange')
+//       break;
+//     case 'group-btn':
+//         yoga.classList.add('hidden');
+//         yogaBtn.classList.remove('bg-orange')
 
-        solo.classList.add('hidden');
-        soloBtn.classList.remove('bg-orange')
+//         group.classList.remove('hidden');
+//         groupBtn.classList.add('bg-orange');
 
-        stretch.classList.remove('hidden');
-        stretchBtn.classList.add('bg-orange');
-        break;
-    default:
+//         solo.classList.add('hidden');
+//         soloBtn.classList.remove('bg-orange')
 
-          break;
-   }
+//         stretch.classList.add('hidden');
+//         stretchBtn.classList.remove('bg-orange');
+//         break;
+//     case 'solo-btn':
+//         yoga.classList.add('hidden');
+//         yogaBtn.classList.remove('bg-orange')
+
+//         group.classList.add('hidden');
+//         groupBtn.classList.remove('bg-orange')
+
+//         solo.classList.remove('hidden');
+//         soloBtn.classList.add('bg-orange')
+
+//         stretch.classList.add('hidden');
+//         stretchBtn.classList.remove('bg-orange');
+//         break;
+//     case 'stretch-btn':
+//         yoga.classList.add('hidden');
+//         yogaBtn.classList.remove('bg-orange')
+
+//         group.classList.add('hidden');
+//         groupBtn.classList.remove('bg-orange')
+
+//         solo.classList.add('hidden');
+//         soloBtn.classList.remove('bg-orange')
+
+//         stretch.classList.remove('hidden');
+//         stretchBtn.classList.add('bg-orange');
+//         break;
+//     default:
+
+//           break;
+//    }
   
-})
+// })
 
 
 
-// BMI Calculator
+// // BMI Calculator
 
-const heightInput = document.querySelector('#c-height');
-const weightInput = document.querySelector('#c-weight');
-const bmiSpan = document.querySelector('#bmi-span');
-const bmiIcon = document.querySelector('.scroll-icon');
+// const heightInput = document.querySelector('#c-height');
+// const weightInput = document.querySelector('#c-weight');
+// const bmiSpan = document.querySelector('#bmi-span');
+// const bmiIcon = document.querySelector('.scroll-icon');
 
-// // BMI Calculator Functions
+// // // BMI Calculator Functions
 
-function calculateBMI() {
+// function calculateBMI() {
 
-    // Input değerlerini dönüştür
-    const heightM = parseFloat(heightInput.value) / 100;
-    const weightKG = parseFloat(weightInput.value);
+//     // Input değerlerini dönüştür
+//     const heightM = parseFloat(heightInput.value) / 100;
+//     const weightKG = parseFloat(weightInput.value);
 
-    // Boş değerleri kontrol et
-    if (heightInput.value === '' || weightInput.value === '') {
-        return;
-    } else if (heightInput.value == 0 || weightInput.value == 0) {
-        return;
-    } 
+//     // Boş değerleri kontrol et
+//     if (heightInput.value === '' || weightInput.value === '') {
+//         return;
+//     } else if (heightInput.value == 0 || weightInput.value == 0) {
+//         return;
+//     } 
     
-    // Formül
-    const bmi = weightKG / (heightM * heightM);
-    bmiSpan.textContent = bmi.toFixed(2);
+//     // Formül
+//     const bmi = weightKG / (heightM * heightM);
+//     bmiSpan.textContent = bmi.toFixed(2);
 
-    // Pozisyon ayarlama
-    let position = 0;
+//     // Pozisyon ayarlama
+//     let position = 0;
 
-    if (bmi < 18.5) {
-        position = 10; 
-    } else if (bmi >= 18.5 && bmi <= 24.9) {
-        position = 30; 
-    } else if (bmi >= 25 && bmi <= 29.9) {
-        position = 50; 
-    } else if (bmi >= 30 && bmi <= 34.9) {
-        position = 70; 
-    } else if (bmi >= 35) {
-        position = 90; 
-    }
+//     if (bmi < 18.5) {
+//         position = 10; 
+//     } else if (bmi >= 18.5 && bmi <= 24.9) {
+//         position = 30; 
+//     } else if (bmi >= 25 && bmi <= 29.9) {
+//         position = 50; 
+//     } else if (bmi >= 30 && bmi <= 34.9) {
+//         position = 70; 
+//     } else if (bmi >= 35) {
+//         position = 90; 
+//     }
 
-    // Iconu göster
-    bmiIcon.style.left = `${position}%`; 
-    bmiIcon.classList.remove('hidden');
-}
+//     // Iconu göster
+//     bmiIcon.style.left = `${position}%`; 
+//     bmiIcon.classList.remove('hidden');
+// }
 
-// // BMI Calculator Events
-heightInput.addEventListener('input', calculateBMI);
-weightInput.addEventListener('input', calculateBMI);
+// // // BMI Calculator Events
+// heightInput.addEventListener('input', calculateBMI);
+// weightInput.addEventListener('input', calculateBMI);
 
--- src/main.js
+++ src/main.js
@@ -1,24 +1,162 @@
-// import './style.css'
-// import javascriptLogo from './javascript.svg'
-// import viteLogo from '/vite.svg'
-// import { setupCounter } from './counter.js'
+// Navbar
+const navbar = document.querySelector('#header');
 
-// document.querySelector('#app').innerHTML = `
-//   <div>
-//     <a href="https://vite.dev" target="_blank">
-//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
-//     </a>
-//     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-//       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-//     </a>
-//     <h1>Hello Vite!</h1>
-//     <div class="card">
-//       <button id="counter" type="button"></button>
-//     </div>
-//     <p class="read-the-docs">
-//       Click on the Vite logo to learn more
-//     </p>
-//   </div>
-// `
+// // Scroll event
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 70) {
+    navbar.classList.add('navbar-scroll');
+  } else {
+    navbar.classList.remove('navbar-scroll');
+  }
+});
 
-// setupCounter(document.querySelector('#counter'))
+// // Hamburger Menu 
+const hamburger = document.querySelector('#nav-button');
+const navMenu = document.querySelector('#burger-menu');
+
+// //  Events - Hamburger Menu
+hamburger.addEventListener('click', () => {
+    if (navMenu.classList.contains('hidden')) {
+        navMenu.classList.remove('hidden');
+    } else {
+        navMenu.classList.add('hidden');
+    }
+});
+
+
+// Our Classes Section
+
+const yoga = document.querySelector('.yoga');
+const yogaBtn = document.querySelector('#yoga-btn');
+
+const group = document.querySelector('.group');
+const groupBtn = document.querySelector('#group-btn');
+
+const solo = document.querySelector('.solo');
+const soloBtn = document.querySelector('#solo-btn');
+
+const stretch = document.querySelector('.stretch');
+const stretchBtn = document.querySelector('#stretch-btn');
+
+const classesBtns = document.querySelector('.classes-btns');
+
+
+// Our Classes Section Events
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('yoga-btn').click();
+  });
+
+classesBtns.addEventListener('click', (e) => {
+  const id = e.target.id;
+  switch (id) {
+    case 'yoga-btn':
+      yoga.classList.remove('hidden');
+      yogaBtn.classList.add('bg-orange')
+
+      group.classList.add('hidden');
+      groupBtn.classList.remove('bg-orange')
+
+      solo.classList.add('hidden');
+      soloBtn.classList.remove('bg-orange')
+
+      stretch.classList.add('hidden');      
+      stretchBtn.classList.remove('bg-orange')
+      break;
+    case 'group-btn':
+        yoga.classList.add('hidden');
+        yogaBtn.classList.remove('bg-orange')
+
+        group.classList.remove('hidden');
+        groupBtn.classList.add('bg-orange');
+
+        solo.classList.add('hidden');
+        soloBtn.classList.remove('bg-orange')
+
+        stretch.classList.add('hidden');
+        stretchBtn.classList.remove('bg-orange');
+        break;
+    case 'solo-btn':
+        yoga.classList.add('hidden');
+        yogaBtn.classList.remove('bg-orange')
+
+        group.classList.add('hidden');
+        groupBtn.classList.remove('bg-orange')
+
+        solo.classList.remove('hidden');
+        soloBtn.classList.add('bg-orange')
+
+        stretch.classList.add('hidden');
+        stretchBtn.classList.remove('bg-orange');
+        break;
+    case 'stretch-btn':
+        yoga.classList.add('hidden');
+        yogaBtn.classList.remove('bg-orange')
+
+        group.classList.add('hidden');
+        groupBtn.classList.remove('bg-orange')
+
+        solo.classList.add('hidden');
+        soloBtn.classList.remove('bg-orange')
+
+        stretch.classList.remove('hidden');
+        stretchBtn.classList.add('bg-orange');
+        break;
+    default:
+
+          break;
+   }
+  
+})
+
+
+
+// BMI Calculator
+
+const heightInput = document.querySelector('#c-height');
+const weightInput = document.querySelector('#c-weight');
+const bmiSpan = document.querySelector('#bmi-span');
+const bmiIcon = document.querySelector('.scroll-icon');
+
+// // BMI Calculator Functions
+
+function calculateBMI() {
+
+    // Input değerlerini dönüştür
+    const heightM = parseFloat(heightInput.value) / 100;
+    const weightKG = parseFloat(weightInput.value);
+
+    // Boş değerleri kontrol et
+    if (heightInput.value === '' || weightInput.value === '') {
+        return;
+    } else if (heightInput.value == 0 || weightInput.value == 0) {
+        return;
+    } 
+    
+    // Formül
+    const bmi = weightKG / (heightM * heightM);
+    bmiSpan.textContent = bmi.toFixed(2);
+
+    // Pozisyon ayarlama
+    let position = 0;
+
+    if (bmi < 18.5) {
+        position = 10; 
+    } else if (bmi >= 18.5 && bmi <= 24.9) {
+        position = 30; 
+    } else if (bmi >= 25 && bmi <= 29.9) {
+        position = 50; 
+    } else if (bmi >= 30 && bmi <= 34.9) {
+        position = 70; 
+    } else if (bmi >= 35) {
+        position = 90; 
+    }
+
+    // Iconu göster
+    bmiIcon.style.left = `${position}%`; 
+    bmiIcon.classList.remove('hidden');
+}
+
+// // BMI Calculator Events
+heightInput.addEventListener('input', calculateBMI);
+weightInput.addEventListener('input', calculateBMI);
+
